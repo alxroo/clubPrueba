870f566d675988c6a3be4d798f7561f9c4346e43
Alex O <alxroo07@gmail.com>
2023-03-04 19:01:39 -0500

    segundo commit

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,18 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
+@main.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+@main.route('/academia')
+def academia():
+    return render_template('academia.html')
+
+@main.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@main.route('/tienda')
+def tienda():
+    return render_template('tienda.html')
