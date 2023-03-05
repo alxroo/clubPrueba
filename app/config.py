900ab5c73510d9eff5686c0f16dd8285a7e8b7f2
alxroo <98622429+alxroo@users.noreply.github.com>
2023-03-04 20:10:25 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,23 +1,23 @@
 import os
 
 class Config():
-    SECRET_KEY = "My_secret_key"
-    FLASKY_ADMIN = "oalxo06@gmail.com"
+    SECRET_KEY = ""
+    FLASKY_ADMIN = "
 
 class DevelopmentConfig(Config):
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = "mysql://root:Al3x0rt1z@localhost/ddbbclubPrueba"
+    SQLALCHEMY_DATABASE_URI = "
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     UPLOAD_FOLDER = os.path.abspath("./app/uploads/")
 
-    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_SERVER = ""
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USE_TLS = False
-    MAIL_USERNAME = "oalxo06@gmail.com"
-    MAIL_PASSWORD = "pjdjklrmfbxukiid"
+    MAIL_USERNAME = ""
+    MAIL_PASSWORD = ""
 
 
 config = {
